82f2ea22ddc184a1e60efc065c628cdb6cb3308b
C_K <80260676+Cks726@users.noreply.github.com>
2023-04-19 22:35:16 -0500

    Ejercicio_2

---
+++ Ejercicio_2.js
@@ -0,0 +1,40 @@
+var pelicula = "Los Locos Addams"
+console.log("Variable película antes del bloque:", pelicula)
+
+let musica = "Rock"
+console.log("Variable músisa antes del bloque:", musica)
+
+{
+    var pelicula="Matilda"
+    console.log("Variable película en el bloque:", pelicula)
+
+    let musica="Salsa"
+    console.log("Variable música en el bloque:", musica)
+}
+
+console.log("Variable película despues del bloque:", pelicula)
+console.log("Variable música despues del bloque:", musica)
+
+
+var nombre = "Pepito"
+var apellido = "Perez"
+
+let edad = "38"
+let comida = "pizza"
+
+{
+    var nombre ="fulano"
+    var apellido = "de tal"
+    let edad = "40"
+    let comida = "Ajiaco"
+
+    console.log("la variable global se puede leer aqui", nombre)
+    console.log("la variable global se puede leer aqui", apellido)
+    console.log("la variable global se puede leer aqui", edad)
+    console.log("la variable global se puede leer aqui", comida)
+}
+
+console.log("la variable global se puede leer aqui", nombre)
+console.log("la variable global se puede leer aqui", apellido)
+console.log("la variable global se puede leer aqui", edad)
+console.log("la variable global se puede leer aqui", comida)
