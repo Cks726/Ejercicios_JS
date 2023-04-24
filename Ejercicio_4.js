07134c13ef1a0f8a125dfac0fd0429dc7bb9344e
C_K <80260676+Cks726@users.noreply.github.com>
2023-04-23 21:47:34 -0500

    Ejercicio_4

---
+++ Ejercicio_4.js
@@ -0,0 +1,13 @@
+// Ejercicio 4
+// tipos de datos en programación.
+
+let cadenaTexto = "Hola Mundo";
+
+console.log(cadenaTexto) 
+console.log(typeof(cadenaTexto)) 
+
+let nombre = "Kamilo";
+cadenaTexto= "Hola!" + " Mi nombre es " + nombre
+console.log(cadenaTexto)
+
+
