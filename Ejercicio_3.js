c7052db5d89f6793040b46cc39f374eef6eee626
C_K <80260676+Cks726@users.noreply.github.com>
2023-04-23 20:16:38 -0500

    Ejercicio_3

---
+++ Ejercicio_3.js
@@ -0,0 +1,22 @@
+// Ejercicio 3
+// Definicion de constante e imprimir por consola
+
+const PI=3.1416;
+//Otra manera de concatenar texto es: `Texto ${variable / constante}`
+console.log(`El valor de PI es: ${PI}`)
+
+// Método 1 - Mi método antes de ver el método del Profesor
+
+const a = 60;
+const c = 24;
+
+console.log("Un minuto tiene;",a, "segundos,", "Una hora tiene:", a, "minutos", "Y un día tiene:", c, "horas")
+
+// Método 2 - Método del Profesor
+
+const segundos_en_minuto = 60;
+const minutos_en_hora = 60;
+const horas_en_dia = 24;
+
+console.log(`Un minuto tiene ${segundos_en_minuto} segundos, una hora tiene ${minutos_en_hora} minutos, un día tiene ${horas_en_dia} horas` )
+
